90ce863990f273c5556504626db7ca9a021b753a
derekriemer <derek.riemer@colorado.edu>
2016-10-24 18:38:25 -0600

    Fix readme, add beeps

--- addon/globalPlugins/synthUnprofile/__init__.py
+++ addon/globalPlugins/synthUnprofile/__init__.py
@@ -1,4 +1,4 @@
-# SynthUnprofile: An Add-on for nvda that does <Insert thing here>
+# SynthUnprofile: An Add-on for nvda that makes the synth settings ring intuitive
 #Copyright (C) 2016 derek riemer
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
@@ -9,20 +9,34 @@ A global plugin to make the synth settings ring intuitive.
 
 import addonHandler
 import config
-import synthSettingsRing
 import globalVars
-import synthDriverHandler
 import globalPluginHandler
+import synthDriverHandler
+import synthSettingsRing
+import tones
 #We need to initialize translation and localization support:
 addonHandler.initTranslation()
 
-oldSynthSetting = synthSettingsRing.SynthSetting
-class SynthSetting(oldSynthSetting):
+class SynthSetting(synthSettingsRing.SynthSetting):
 	def _set_value(self,value):
 		setattr(self.synth,self.setting.name,value)
 		config.conf.profiles[0]["speech"][self.synth.name][self.setting.name]=value
 
+class SynthSettingsRing(synthSettingsRing.SynthSettingsRing):
+	def _get_currentSettingName(self):
+		""" returns the current setting's name """
+		if self._current is not None and hasattr(self,'settings'):
+			name =  self.settings[self._current].setting.displayName
+			if isinstance(self.settings[self._current], synthSettingsRing.StringSynthSetting):
+				tones.beep(80, 80)
+			return name
+		return None
+
+
 synthSettingsRing.SynthSetting = SynthSetting
+synthSettingsRing.SynthSettingsRing = SynthSettingsRing
+
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
--- readme.md
+++ readme.md
@@ -6,3 +6,4 @@ To build this addon, please visit [building.md]
 SynthUnprofile allows you to make the synth settings ring back to what it should be, where the changes apply to NVDA's global config as it should, which is much more useful. 
 To use, simply instal the addon, and use the synth settings ring as you'd use it normally.
 There's one caviot, which is that it doesn't work with changing voice. This is complicated and I would have to hack nvda in a potentially harmful way to fix it.
+It beeps at you if this change would modify the profile though.
